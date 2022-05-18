ca34c01d43a262a3fa12da80d1756281e81d8abf
natiman58 <natiman58@gmail.com>
2022-05-17 22:58:10 -0700

    removed builtins

--- shell.c
+++ shell.c
@@ -4,36 +4,6 @@
 #include<string.h>
 #include<sys/wait.h>
 
-/**
- * sh_cd - the variable that holds the command cd
- * sh_help - help page
- * sh_exit - the variable holds the command exit
- * @args: Null terminated array of words passed in
- * Return: 1
- */
-
-int sh_cd(char **args);
-int sh_help(char **args);
-int sh_exit(char **args);
-
-/**
- * builtin_str[]- builtin commands
- */
-char(*builtin_str[]) = {
-"cd",
-"help",
-"exit",
-};
-/**
- * int *builtin_func[] - corresponding builtin functions
- * @&sh_cd: holds the command cd
- * @&sh_help: holds the command help
- */
-int (*builtin_func[]) (char **) = {
-&sh_cd,
-&sh_help,
-&sh_exit,
-};
 
 /**
  * sh_num_builtins - number of builtin commands
@@ -41,63 +11,8 @@ int (*builtin_func[]) (char **) = {
  */
 int sh_num_builtins(void)
 {
-return (sizeof(builtin_str) / sizeof(char *));
-}
-
-/**
- * sh_cd - changes dir using the chdir function
- * @args: the arguments passed into stdin
- * Return: 1;
- */
-int sh_cd(char **args)
-{
-if (args[1] == NULL)
-{
-fprintf(stderr, "sh: expected argument to \"cd\"\n");
-}
-else
-{
-if (chdir(args[1]) != 0)
-{
-perror("sh");
-}
-}
-return (1);
-}
-/**
- * sh_help - help page
- * @args: void arguments
- * Return: 1
- */
-int sh_help(char **args)
-{
-int i = 0;
-(void)args;
-
-
-printf("My super simple shell\n");
-printf("Type the command and hit enter\n");
-printf("The following are built in:\n");
-
-for (i = 0; i < sh_num_builtins(); i++)
-{
-printf(" %s\n", builtin_str[i]);
-}
-printf("Use the man command for info on other programs\n");
-return (1);
-}
-
-/**
- * sh_exit - exit function
- * @args: void arguments
- * Return: 0
- */
-int sh_exit(char **args)
-{
-(void)args;
 return (0);
 }
-
 /**
  * sh_launch - launches the processes
  * and wait for the child to exit.
@@ -139,19 +54,9 @@ return (1);
  */
 int sh_execute(char **args)
 {
-int i;
-
 if (args[0] == NULL)
 {
 return (1);
-}
-
-for (i = 0; i < sh_num_builtins(); i++)
-{
-if (strcmp(args[0], builtin_str[i]) == 0)
-{
-return (builtin_func[i] (args));
-}
 }
 return (sh_launch(args));
 }
